0000646abc9e86bd53fe5bec4a1263754a2ce87e
Gold <proningm@gmail.com>
2020-10-16 17:40:09 +0300

    Добавляем JS. Счетчик лайков.

---
+++ js/like-count.js
@@ -0,0 +1,15 @@
+// cоздание Лайков и их счетчика. 
+
+let hend = document.querySelector('.like__img');              // элемент поднятого пальца
+let likesNumber = document.querySelector('.like__count');     // цифра счетчика в html>span
+let counter = 16;                                            // переменна для счетчика лайков - отсчет с 16 как в проекте
+
+hend.onclick = function () {                                // фкнкция нажития на сердчко
+    if (hend.classList.contains('like-good')) {            // если лайк был до нажатия, то
+        likesNumber.textContent--;
+        hend.classList.remove('like-good');                // счетчик уменьшится на 1 единицу, и удалится класс с изменением фона картинки
+    } else {                                               // функция - Если условие - не верно 
+        likesNumber.textContent++;
+        hend.classList.add('like-good');               // счетчик увеличится на 1 единицу, и добавится класс с изменением фона 
+    }
+};
