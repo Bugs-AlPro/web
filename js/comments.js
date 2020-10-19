be6e9b80d572618c61f1e919d4f9be79ee196683
Gold <proningm@gmail.com>
2020-10-19 20:58:01 +0300

    JS. Доработан раздел отправки комментарией: цвет текста и подсчет символов.

--- js/comments.js
+++ js/comments.js
@@ -1,7 +1,8 @@
 let commentForm = document.querySelector('.comment-f__form');  // форма добавления комментария
 let commentList = document.querySelector('.comments__list');          // список новых комментариев
 let commentUserName = document.querySelector('#name-user');    // имя пользователя
-let commentField = document.querySelector('.comment-f__text');    // поле ввода
+let commentField = document.querySelector('.comment-f__text');    // поле ввода комментария
+let commentUserMail = document.querySelector('#email-user');      // поле ввода почты полльзователя
 let submitButton = document.querySelector('.contact-f__button'); // находим кнопку отправки комментария
 
 commentForm.onsubmit = function (evt) {                // устанавливаем обработку события на отправку формы
@@ -19,9 +20,24 @@ commentForm.onsubmit = function (evt) {                // устанавлива
     let newImg = document.createElement('img');        // создаем элемент аватара 
     newImg.classList.add('comment__img');              // добавляем новому элементу классы
     newImg.setAttribute('src', 'img/newAvatar.png');   // добавляем аватару источкин для картинки
+    newImg.setAttribute('alt', 'Аватар автора комментария');   // добавляем аватару атрибут alt
     let newUserName = document.createElement('p');     // создаём элемент содержащий имя автора комментарий
     newUserName.classList.add('comment__user-name');   // добавляем новому элементу классы
     newUserName.textContent = commentUserName.value;   // добавляем элементу текст из поля ввода 
     newContainer.append(newImg, newUserName);          // добавляем аватар и имя пользователя в контейнер
     commentList.append(newComment);                    // добавляем новый комментарий в DOM
+    commentField.value = '';                           // очищаем поле ввода комментария
+    commentUserName.value = '';                        // очищаем поле ввода имя полььзователя
+    commentUserMail.value = '';                        // очищаем поле ввода Email
+}
+
+commentField.oninput = function () {                   // обработка события на ввод данных
+    if (commentField.value.length < 10 || commentField.value.length > 200) {    // условие соответсвия на количество символов
+        commentField.classList.add('red-text');        // добавление класса, на изменение цвета текста
+        submitButton.disabled = true;                 // фиксируем кнопку в положении - отключена
+    }
+    else {
+        submitButton.disabled = false;                 // включаем кнопку отправки форы
+        commentField.classList.remove('red-text');      // удаляем выделение цвета текста красным
+    }
 }
